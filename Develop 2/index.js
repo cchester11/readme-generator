8cc63ff9b2efbbf5b5cdd10679c45a24276c69c1
Charles <cchester1@angelo.edu>
2021-04-07 17:23:18 -0500

    ..

--- Develop 2/index.js
+++ Develop 2/index.js
@@ -1,10 +1,30 @@
 // TODO: Include packages needed for this application
-
+inquirer = require('inquirer');
+fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  },
+  {
+    type: 'checkbox',
+    name: 'languages',
+    message: 'Whats your favorite language?',
+    choices: ['HTML', 'CSS', 'Javascript', 'Python']
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile('README.md', questions, function(error) {
+    if(error) {
+      return new error
+    }
+    console.log(questions)
+  })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop 2/utils/generateMarkdown.js
+++ Develop 2/utils/generateMarkdown.js
@@ -4,7 +4,13 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license) {
+    return renderLicenseLink;
+  } else {
+    return console.error();
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
